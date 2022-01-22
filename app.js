3d612ef41d6e7efb6a0893a0a12913a771065625
Jenny Zonka <jennycade@gmail.com>
2022-01-22 11:40:03 -0800

    Add a route

--- app.js
+++ app.js
@@ -4,23 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var coolRouter = require('./routes/cool');
 
+// start express
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routing
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cool', coolRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +44,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// export
 module.exports = app;
