b7d89096f24a76b68c5555dbd93f85ef524715f9
Jenny Zonka <jennycade@gmail.com>
2022-01-29 15:01:10 -0800

    Format dates in book instances list

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
 
+// for date formatting
+const { DateTime } = require('luxon');
+
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
@@ -16,6 +19,13 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function() {
+  return DateTime.fromJSDate(this.due_back)
+    .toLocaleString(DateTime.DATE_MED)
 });
 
 //Export model
