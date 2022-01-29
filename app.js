b1fa5d88bad469344c1fb88e7511ef04e55d14b5
Jenny Zonka <jennycade@gmail.com>
2022-01-29 13:45:15 -0800

    Set up catalog routes and dumy route handlers

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var coolRouter = require('./routes/cool');
+let catalogRouter = require('./routes/catalog');
 
 // start express
 var app = express();
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cool', coolRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
