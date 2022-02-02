85a06401dbbbefd849e241b7c63511f3dae50309
Jenny Zonka <jennycade@gmail.com>
2022-02-02 13:37:13 -0800

    Added delete author form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -144,13 +144,73 @@ exports.author_create_post = [
 ];
 
 // Display Author delete form on GET.
-exports.author_delete_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Author delete GET');
+exports.author_delete_get = function(req, res, next) {
+  async.parallel(
+    {
+      author: (callback) => {
+        Author.findById(req.params.id).exec(callback);
+      },
+
+      authors_books: (callback) => {
+        Book.find({'author': req.params.id }).exec(callback);
+      },
+    },
+
+    (err, results) => {
+      if (err) { return next(err); }
+      if (results.author === null) {
+        res.redirect('/catalog/authors');
+      }
+
+      res.render(
+        'author_delete',
+        {
+          title: 'Delete Author',
+          author: results.author,
+          author_books: results.authors_books,
+        }
+      );
+    }
+  );
 };
 
 // Handle Author delete on POST.
-exports.author_delete_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Author delete POST');
+exports.author_delete_post = function(req, res,next) {
+  
+  async.parallel(
+    {
+      author: (callback) => {
+        Author.findById(req.body.authorid).exec(callback);
+      },
+      authors_books: (callback) => {
+        Book.find({author: req.body.authorid}).exec(callback);
+      }
+    },
+
+    (err, results) => {
+      if (err) { return next(err) }
+      if (results.authors_books.length > 0) {
+        // Author has books. render as GET
+        res.render('author_delete',
+          {
+            title: 'Delete Author',
+            author: results.author,
+            author_books: results.authors_books,
+          }
+        );
+        return;
+      } else {
+        // no books, delete author
+        Author.findByIdAndRemove(
+          req.body.authorid,
+          (err) => {
+            if (err) { return next(err); }
+            res.redirect('/catalog/authors');
+          }
+        );
+      }
+    }
+  );
 };
 
 // Display Author update form on GET.
